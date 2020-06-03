03827f27f454103a8c5a3a38d5dc680707534fe9
akshitgupta1109 <66363632+akshitgupta1109@users.noreply.github.com>
2020-06-03 22:15:36 +0530

    Add files via upload

---
+++ Fetch_Patient_Data_Trigger.bat
@@ -0,0 +1,5 @@
+ECHO ON
+"C:\Users\nm17839\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\nm17839\Documents\Automation Anywhere Files\Automation Anywhere\My Docs\Fetch_Patient_Data.py"
+
+
+
---
+++ Post_Appointment_Details_Trigger.bat
@@ -0,0 +1,2 @@
+ECHO ON
+"C:\Users\nm17839\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\nm17839\Documents\Automation Anywhere Files\Automation Anywhere\My Docs\Post_Appointment_Details.py"
